a1f8f23ae31c0ad10204a91a0d4c5fccc51e7d25
Poonam Gaigole <poonam.gaigole@axelerant.com>
2024-11-12 16:38:50 +0530

    Added a scoreboard table

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -5,11 +5,21 @@ export default function ScoreCard({ info }) {
   const { rolls, minutes, seconds } = info;
   return (
     <section className="scorecard-section">
-      <h3>
-        {rolls} rolls -{" "}
-        {parseInt(minutes) > 0 && `${minutes} minute${minutes > 1 ? "s" : ""} `}
-        {seconds} second{seconds > 1 ? "s" : ""}
-      </h3>
+      <h2>Top scores</h2>
+      <table>
+        <tr>
+          <th>Rolls</th>
+          <th>Time</th>
+        </tr>
+        <tr>
+          <td>{rolls}</td>
+          <td>
+            {parseInt(minutes) > 0 &&
+              `${minutes} minute${minutes > 1 ? "s" : ""} `}
+            {seconds} second{seconds > 1 ? "s" : ""}
+          </td>
+        </tr>
+      </table>
     </section>
   );
 }
