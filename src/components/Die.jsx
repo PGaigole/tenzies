f186623aa36ee806350eeb39a1bca527b0d8c810
Poonam Gaigole <poonam.gaigole@axelerant.com>
2024-11-12 09:07:07 +0530

    Changed dice numbers into dots

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -4,9 +4,17 @@ export default function Die({ value, isHeld, click, id }) {
   const styles = {
     backgroundColor: isHeld ? "#59e391" : "#fff",
   };
+  const dots = Array.from({ length: value });
   return (
-    <div className="die-container" style={styles} onClick={() => click(id)}>
-      <h2>{value}</h2>
+    <div
+      className="die-container"
+      style={styles}
+      onClick={() => click(id)}
+      data-value={value}
+    >
+      {dots.map((_, index) => (
+        <span key={index} className="dot"></span>
+      ))}
     </div>
   );
 }
